195c67c9c766e98020a5460abd7b2e451d9f2457
zeek0x <0h23k1.n40k1@gmail.com>
2022-03-13 04:10:04 +0900

    Add srv.py

---
+++ srv.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello CORS!')
+        return
+
+httpd = HTTPServer(('localhost', 8003), CORSRequestHandler)
+httpd.serve_forever()
