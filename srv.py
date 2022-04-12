42848341d485182eaf21cbd989fb7928b4c6e17d
zeek0x <0h23k1.n40k1@gmail.com>
2022-04-12 21:35:48 +0900

    Add acao header with wildcard

--- srv.py
+++ srv.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(b'Hello CORS!')
         return
