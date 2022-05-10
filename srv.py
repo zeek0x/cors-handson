f64571982834dda20f9e2b1035a86001bad8b0a0
zeek0x <0h23k1.n40k1@gmail.com>
2022-05-10 21:09:17 +0900

    Restore from tmp code

--- srv.py
+++ srv.py
@@ -9,9 +9,8 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         origin = self.headers['Origin']
-        print(origin)
         acao = origin if self.is_valid_origin(origin) else ' '.join(self.valid_origin_list)
-        self.send_header('Access-Control-Allow-Origin', 'https://example.com',)
+        self.send_header('Access-Control-Allow-Origin', acao)
         self.end_headers()
         self.wfile.write(b'Hello CORS!')
         return
